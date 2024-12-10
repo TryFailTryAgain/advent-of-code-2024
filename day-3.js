aaae853fd6b3b8845ebf572f0ca81fe0440353a2
TryFailTryAgain <115508207+TryFailTryAgain@users.noreply.github.com>
2024-12-10 12:58:57 -0500

    Day 3 Part 2

--- day-3.js
+++ day-3.js
@@ -5,25 +5,75 @@
 
 const fs = require('fs');
 
-let commands = prepareData();
-console.log("Part 1: ", part1(commands));
-//console.log("Part 2: ", part2(reports));
-
+let { mul, dos, donts } = prepareData();
+console.log("Part 1: ", part1(mul));
+console.log("Part 2: ", part2(mul, dos, donts));
 
 function prepareData() {
     const inputData = fs.readFileSync('input-3.txt', 'utf8');
-    // Splits the data into commands
-    let data = inputData.match(/mul\(\d{1,3},\d{1,3}\)/g)
-    // Parses the commands into number pairs for multiplication, then takes each pair and ensures they are ints
-    data = data.map(multiplier => multiplier.match(/\d{1,3}/g).map(num => parseInt(num)));
-    return data;
+    const mulRegex = /mul\(\d{1,3},\d{1,3}\)/g;
+    const doRegex = /do\(\)/g;
+    const dontRegex = /don't\(\)/g;
+    let match = null;
+    let mul = [];
+    let dos = [];
+    let donts = [];
+
+    while ((match = mulRegex.exec(inputData)) !== null) {
+        mul.push({
+            match: match[0],
+            index: match.index
+        });
+    }
+    while ((match = doRegex.exec(inputData)) !== null) {
+        dos.push({
+            match: match[0],
+            index: match.index
+        });
+    }
+    while ((match = dontRegex.exec(inputData)) !== null) {
+        donts.push({
+            match: match[0],
+            index: match.index
+        });
+    }
+    // Parses the mul commands into number pairs for multiplication, then takes each pair and ensures they are ints
+    mul.map(mul => {
+        let pair = mul.match.match(/\d{1,3}/g);
+        mul.match = [parseInt(pair[0]), parseInt(pair[1])];
+    });
+    return { mul, dos, donts };
 }
 
-function part1(commands){
+function part1(mul) {
     // Multiplies the pairs of numbers in the commands
     let total = 0;
-    for (let i = 0; i < commands.length; i++){
-        total += commands[i][0] * commands[i][1];
+    mul.map(mul => {
+        total += mul.match[0] * mul.match[1];
+    });
+    return total;
+}
+
+function part2(mul, dos, donts) {
+    // Multiplies the number pairs only if they are after a do() command but not after a don't() command
+    let total = 0;
+    // mul commands start off accepted
+    let counting = true
+
+    // Concats all the matches and sorts them by their index
+    let allMatches = mul.concat(dos, donts);
+    allMatches.sort((a, b) => a.index - b.index);
+
+    for (let i = 0; i < allMatches.length; i++) {
+        if (allMatches[i].match === "do()") {
+            counting = true;
+        }
+        if (allMatches[i].match === "don't()") {
+            counting = false;
+        }
+        if (counting == true && allMatches[i].match !== "do()" && allMatches[i].match !== "don't()") {
+            total += allMatches[i].match[0] * allMatches[i].match[1];
+        }
     }
     return total;
 }
