9b5eacad83b64bbbc705c899be01d0ede5140225
TryFailTryAgain <115508207+TryFailTryAgain@users.noreply.github.com>
2024-12-10 15:47:14 -0500

    Create day-5.js

---
+++ day-5.js
@@ -0,0 +1,5 @@
+// Author: TryFailTryAgain
+// Copyright (c) 2024. All rights reserved. For use in Open-Source projects this
+// may be freely copied or excerpted with credit to the author.
+// Advent of Code 2024 Day 5 part 1-2
+
